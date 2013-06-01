0e9353e12d8e14e98003477fd50b7723a10699a9
Denis Doria <denisdoria@gmail.com>
2013-06-01 08:04:28 +0200

    Included the possibility of reading files in block

--- basic.py
+++ basic.py
@@ -1,3 +1,5 @@
+from itertools import groupby
+
 class ProcFile(object):
 
     @property
@@ -7,6 +9,12 @@ class ProcFile(object):
     def _readfile(self):
         with open(self.filename) as opened_file:
             return [line.strip() for line in opened_file.xreadlines() if line.strip() and '#' not in line[0]]
+
+    def _readfile_inblocks(self):
+        with open(self.filename) as opened_file:
+           tmp = [tuple(group) for key,group in itertools.groupby(opened_file, lambda line: line == '\n')]
+
+        return [line for line in tmp if len(line) > 1]
 
     def _writefile(self, line):
         with open(self.filename, 'w+') as opened_file:
