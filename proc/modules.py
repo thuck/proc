dfccd5ca6578f667204afbf903893627c7567717
Denis Doria <denisdoria@gmail.com>
2013-08-17 19:33:50 +0200

    Pep8 and Pylint

--- proc/modules.py
+++ proc/modules.py
@@ -1,11 +1,16 @@
 from .basic import ProcFile
 from collections import namedtuple
 
+
 class Modules(ProcFile):
     filename = '/proc/modules'
     Module = namedtuple('ModuleInfo', ['memory_size',
-                            'instances_counter', 'dependency',
-                            'state', 'memory_offset'])
+                                       'instances_counter',
+                                       'dependency',
+                                       'state',
+                                       'memory_offset'
+                                       ]
+                        )
 
     @property
     def value(self):
@@ -14,7 +19,7 @@ class Modules(ProcFile):
     def names(self):
         return [line.split(' ')[0] for line in self._readfile()]
 
-    def get(self, module_name, default = None):
+    def get(self, module_name, default=None):
         for line in self._readfile():
             line = line.split(' ')
             if line[0] == module_name:
@@ -25,23 +30,24 @@ class Modules(ProcFile):
 
     def __getattr__(self, name):
         if name in self.names():
-            (memory_size,instances_counter,
-            dependency, state, memory_offset) = tuple(self.get(name))
+            (memory_size, instances_counter,
+             dependency, state, memory_offset
+             ) = tuple(self.get(name))
             return self.Module(int(memory_size), int(instances_counter),
-                                dependency, state, memory_offset)
+                               dependency, state, memory_offset
+                               )
 
         else:
-            raise AttributeError 
+            raise AttributeError
 
 
 if __name__ == '__main__':
-    modules = Modules()
-    #print modules.value
-    print(modules.names())
-    print(modules.get('i915'))
-    print(modules.get('i915a'))
-    print(modules.i915.memory_offset)
-    print(modules.i915.instances_counter)
-    print(modules.i915.dependency)
-    print(modules.i915.state)
-    print(modules.i915.memory_size)
+    MODULES = Modules()
+    print(MODULES.names())
+    print(MODULES.get('i915'))
+    print(MODULES.get('i915a'))
+    print(MODULES.i915.memory_offset)
+    print(MODULES.i915.instances_counter)
+    print(MODULES.i915.dependency)
+    print(MODULES.i915.state)
+    print(MODULES.i915.memory_size)
